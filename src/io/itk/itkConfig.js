b91c5fc46a379f7ef03755034f3c721bbb563884
Moonsic <2270374117@qq.com>
2024-03-07 10:45:04 +0800

    生产环境路径改成/static/volview-dist/itk/xxx 才能被A项目访问到

--- src/io/itk/itkConfig.js
+++ src/io/itk/itkConfig.js
@@ -5,13 +5,17 @@ const base = import.meta.env.BASE_URL;
 const fullUrl = (relative) =>
   new URL(join(base, relative), document.location.origin).href;
 
-// GGG 路径改成/static/volview-dist/itk/才能被A项目访问到
+// GGG 生产环境路径改成/static/volview-dist/itk/xxx 才能被A项目访问到
+
+const isProd = process.env.NODE_ENV === 'production'; // 是否为生产环境
+
+const path = isProd ? '/static/volview-dist' : ''
 
 const itkConfig = {
-  pipelineWorkerUrl: fullUrl('/static/volview-dist/itk/itk-wasm-pipeline.min.worker.js'),
-  imageIOUrl: fullUrl('/static/volview-dist/itk/image-io'),
-  meshIOUrl: fullUrl('/static/volview-dist/itk/mesh-io'),
-  pipelinesUrl: fullUrl('/static/volview-dist/itk/pipelines'),
+  pipelineWorkerUrl: fullUrl(`${path}/itk/itk-wasm-pipeline.min.worker.js`),
+  imageIOUrl: fullUrl(`${path}/itk/image-io`),
+  meshIOUrl: fullUrl(`${path}/itk/mesh-io`),
+  pipelinesUrl: fullUrl(`${path}/itk/pipelines`),
 };
 
 export default itkConfig;
